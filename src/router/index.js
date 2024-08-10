1224a63df623184cb656a4bc63f0c6809af9edc5
vebrian 2019 <vebrian999@gmail.com>
2024-08-10 09:37:14 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+// TODO: Uncomment baris kode dibawah ini untuk meng-import Halaman Hello ke dalam router
+import HelloView from "../views/HelloView.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  // TODO: Uncomment baris kode dibawah ini untuk menambahkan routing baru ke Halaman Hello
+  {
+    path: "/hello",
+    name: "hello",
+    component: HelloView,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
